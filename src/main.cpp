65f40d829426d205cfa56ad05bb7a49a60ffc062
SlimRunner <dv.polygon@gmail.com>
2024-07-10 02:55:51 -0700

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,20 @@
 #include "MinHeap.hpp"
+#include "MaxHeap.hpp"
 #include <cstddef> // size_t
 #include <iostream>
 #include <array>
 
-#include <io.h>
-#include <fcntl.h>
-
 int main(int, char **) {
   std::array values = {5,3,8,1,9,4};
-  alg::MinHeap<int> heap(values.begin(), values.end());
+  alg::MaxHeap<int> heap(values.begin(), values.end());
   heap.insert(69);
   heap.insert(57);
   heap.insert(-4);
   heap.insert(-300);
 
   std::cout << std::endl;
-  std::cout << heap.toString(alg::TreeStyle::V_BOXED);
+  std::cout << heap.toString(alg::TreeStyle::H_BOXED);
+  // std::cout << heap.toString(alg::TreeStyle::V_BOXED);
   std::cout << std::endl;
 
 // #if _WIN32
