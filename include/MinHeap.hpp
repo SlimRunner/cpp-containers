b359a02ab8706bb1f66f0d73b68e0ec473f5fd08
SlimRunner <dv.polygon@gmail.com>
2024-07-09 20:49:50 -0700

    Implement to string in min heap

---
+++ include/MinHeap.hpp
@@ -0,0 +1,159 @@
+#pragma once
+
+#include <algorithm> // copy, make_heap
+#include <array>
+#include <cmath>
+#include <cstddef> // size_t
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iterator> // back_inserter
+#include <queue>
+#include <sstream>
+#include <string>
+#include <utility> // swap
+#include <vector>
+
+namespace alg {
+
+enum class TreeStyle {
+  V_SPACED,
+  V_BOXED,
+  H_TABBED,
+  H_BOXED,
+};
+
+template <class T> class MinHeap {
+private:
+  std::vector<T> mHeap;
+  void balanceHeap();
+  void heapifyNode(size_t index);
+
+public:
+  MinHeap();
+  template <class It> MinHeap(It begin, It end);
+
+  void insert(T value);
+
+  std::vector<T>::iterator begin() { return mHeap.begin(); }
+  std::vector<T>::iterator end() { return mHeap.end(); }
+
+  std::string toString(TreeStyle style, bool rounded = false) const;
+};
+
+template <class T> void inline MinHeap<T>::balanceHeap() {
+  std::make_heap(mHeap.begin(), mHeap.end(), std::greater<T>{});
+}
+
+template <class T> inline void MinHeap<T>::heapifyNode(size_t index) {
+  bool notSorted = true;
+  // indices here are treated as 1-based
+  size_t iNode = index + 1, iParent = index + 1;
+  do {
+    iNode = iParent;
+    iParent = iNode / 2;
+    notSorted = iParent != 0 && mHeap.at(iNode - 1) < mHeap.at(iParent - 1);
+    if (notSorted) {
+      std::swap(mHeap.at(iNode - 1), mHeap.at(iParent - 1));
+    }
+  } while (notSorted);
+}
+
+template <class T> inline MinHeap<T>::MinHeap() : mHeap{} {}
+
+template <class T> inline void MinHeap<T>::insert(T value) {
+  mHeap.push_back(value);
+  heapifyNode(mHeap.size() - 1);
+}
+
+template <class T>
+inline std::string MinHeap<T>::toString(TreeStyle style, bool rounded) const {
+  const size_t HEIGHT =
+      static_cast<size_t>(std::ceil(std::log2(mHeap.size() + 1)));
+  const size_t HEAP_SIZE = mHeap.size();
+  std::array<std::string, 3> lPipes = {"┌", "┐", "└"};
+  std::array<std::string, 2> endCaps = {"╴", "╶"};
+  std::vector<bool> visited(HEAP_SIZE, false);
+  std::queue<size_t> queue;
+
+  std::function<void(size_t)> enqeue;
+
+  enqeue = [&queue, &enqeue, HEAP_SIZE](size_t i) {
+    i <<= 1;
+    if (i <= HEAP_SIZE) {
+      queue.push(i);
+      enqeue(i);
+    }
+    ++i;
+    if (i <= HEAP_SIZE) {
+      queue.push(i);
+      enqeue(i);
+    }
+  };
+
+  size_t spacing = 0;
+  std::ostringstream oss;
+
+  if (rounded) {
+    lPipes.at(0) = "╭";
+    lPipes.at(1) = "╮";
+    lPipes.at(2) = "╰";
+  }
+
+  for (auto const &item : mHeap) {
+    if (item >= 0) {
+      spacing = static_cast<size_t>(
+          std::max<double>(spacing, std::ceil(std::log10(item + 1))));
+    } else {
+      spacing = static_cast<size_t>(
+          std::max<double>(spacing, 1 + std::ceil(std::log10(-item + 1))));
+    }
+  }
+
+  switch (style) {
+  case TreeStyle::H_BOXED:
+  case TreeStyle::H_TABBED:
+    queue.push(1);
+    visited.at(0) = true;
+    enqeue(1);
+    for (size_t i, lvl = 0; !queue.empty();) {
+      i = queue.front();
+      queue.pop();
+      lvl = static_cast<size_t>(std::ceil(std::log2(i + 1))) - 1;
+      oss << std::setw(0) << std::string(spacing * lvl, ' ');
+      oss << std::setfill('0') << std::setw(spacing) << std::internal;
+      oss << mHeap.at(i - 1) << '\n';
+    }
+
+    break;
+  case TreeStyle::V_BOXED:
+  case TreeStyle::V_SPACED:
+    for (size_t lvl = 0, N0 = 0, N1 = 0; lvl < HEIGHT; ++lvl) {
+      N1 = N0 + (1 << lvl);
+      oss << std::setw(0);
+      oss << std::string(spacing * ((1 << (HEIGHT - lvl - 1)) - 1U), ' ');
+      for (size_t i = N0; i < N1 && i < HEAP_SIZE; ++i) {
+        oss << std::setfill('0') << std::setw(spacing) << std::internal;
+        oss << mHeap.at(i);
+        oss << std::string(spacing * ((1 << (HEIGHT - lvl)) - 1U), ' ');
+      }
+      N0 += N1 - N0;
+      oss << '\n';
+    }
+    break;
+
+  default:
+    break;
+  }
+
+  return oss.str();
+}
+
+template <class T>
+template <class It>
+inline MinHeap<T>::MinHeap(It begin, It end) : MinHeap{} {
+  std::copy(begin, end, std::back_inserter(mHeap));
+  balanceHeap();
+}
+
+} // namespace alg
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
+#include "MinHeap.hpp"
+#include <cstddef> // size_t
 #include <iostream>
+#include <array>
+
+#include <io.h>
+#include <fcntl.h>
 
 int main(int, char **) {
+  std::array values = {5,3,8,1,9,4};
+  alg::MinHeap<int> heap(values.begin(), values.end());
+  heap.insert(69);
+  heap.insert(57);
+  heap.insert(-4);
+  heap.insert(-300);
+
+  std::cout << std::endl;
+  std::cout << heap.toString(alg::TreeStyle::V_BOXED);
+  std::cout << std::endl;
+
+// #if _WIN32
+// #endif
   return 0;
 }
