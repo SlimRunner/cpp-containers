e7b86dd5891d41b6c28ac63ea7b92c824ee2c82e
SlimRunner <dv.polygon@gmail.com>
2024-07-10 16:55:56 -0700

    Implement median heap

--- include/MedHeap.hpp
+++ include/MedHeap.hpp
@@ -1,13 +1,87 @@
 #pragma once
 
+#include "MaxHeap.hpp"
+#include "MinHeap.hpp"
+
 namespace alg {
 
-class MedHeap {
-private:
-  /* data */
+template <class T, class S> class MedHeap {
 public:
-  MedHeap(/* args */);
-  ~MedHeap();
+  MedHeap();
+  template <class It> MedHeap(It begin, It end);
+
+  void insert(T value);
+  S median();
+
+  std::string toString();
+
+private:
+  size_t mSize;
+  MinHeap<T> mTopHeap;
+  MaxHeap<T> mBtmHeap;
+
+  void balanceHeaps();
 };
+
+template <class T, class S> inline MedHeap<T, S>::MedHeap() : mTopHeap{}, mBtmHeap{} {}
+
+template <class T, class S>
+template <class It>
+inline MedHeap<T, S>::MedHeap(It begin, It end) : MedHeap{} {
+  for (It it = begin; it != end; ++it) {
+    insert(*it);
+  }
+}
+
+template <class T, class S> inline void MedHeap<T, S>::insert(T value) {
+  if (mTopHeap.size() == 0 || value > mTopHeap.top()) {
+    mTopHeap.insert(value);
+  } else {
+    mBtmHeap.insert(value);
+  }
+  balanceHeaps();
+  ++mSize;
+}
+
+template <class T, class S> inline S MedHeap<T, S>::median() {
+  if (mTopHeap.size() > mBtmHeap.size()) {
+    return mTopHeap.top();
+  } else if (mTopHeap.size() < mBtmHeap.size()) {
+    return mBtmHeap.top();
+  }
+  return static_cast<S>(mTopHeap.top() + mBtmHeap.top()) / 2;
+}
+
+template <class T, class S> inline std::string MedHeap<T, S>::toString() {
+  std::ostringstream oss;
+  auto top = mTopHeap.toLines(TreeStyle::H_BOXED);
+  auto bottom = mBtmHeap.toLines(TreeStyle::H_BOXED);
+  size_t lineSpan = 0;
+
+  for (auto it = top.rbegin(); it != top.rend(); ++it) {
+    oss << *it << '\n';
+    if (lineSpan < it->length()) {
+      lineSpan = it->length();
+    }
+  }
+  oss << std::string(lineSpan, '-') << '\n';
+  for (auto it = bottom.begin(); it != bottom.end(); ++it) {
+    oss << *it << '\n';
+  }
+
+  return oss.str();
+}
+
+template <class T, class S> inline void MedHeap<T, S>::balanceHeaps() {
+  auto H = mTopHeap.size();
+  auto L = mBtmHeap.size();
+  if (std::abs(static_cast<T>(H - L)) > 1) {
+    if (H > L) {
+      mBtmHeap.insert(mTopHeap.remove());
+    } else {
+      mTopHeap.insert(mBtmHeap.remove());
+    }
+  }
+}
 
 } // namespace alg
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,55 @@
 #include "MaxHeap.hpp"
 #include "MinHeap.hpp"
+#include "MedHeap.hpp"
 #include <array>
 #include <cstddef> // size_t
 #include <iostream>
+#include <iterator>
+
+template <class Iteratable>
+void printVec(const Iteratable &container, bool reversed = false) {
+  if (reversed) {
+    auto begin = std::rbegin(container);
+    auto end = std::rend(container);
+    for (auto it = begin; it != end; ++it) {
+      std::cout << *it << std::endl;
+    }
+  } else {
+    auto begin = std::begin(container);
+    auto end = std::end(container);
+    for (auto it = begin; it != end; ++it) {
+      std::cout << *it << std::endl;
+    }
+  }
+}
 
 int main(int, char **) {
-  std::array values = {5, 3, 8, 1, 9, 4};
-  alg::MaxHeap<int> heap(values.begin(), values.end());
-  heap.insert(69);
-  heap.insert(57);
-  heap.insert(-4);
-  heap.insert(-300);
+  // std::array values = {5, 3, 8, 1, 9, 4};
+  // alg::MinHeap<int> heap(values.begin(), values.end());
+  // heap.insert(69);
+  // heap.insert(57);
+  // heap.insert(-4);
+  // heap.insert(-300);
+  // std::cout << std::endl;
+  // printVec(heap.toLines(alg::TreeStyle::H_BOXED));
+  // std::cout << std::endl;
 
-  std::cout << std::endl;
-  std::cout << heap.toString(alg::TreeStyle::H_BOXED);
-  std::cout << std::endl;
-
-  heap.remove();
-  std::cout << std::endl;
-  std::cout << heap.toString(alg::TreeStyle::H_BOXED);
-  std::cout << std::endl;
-
-  heap.remove();
-  std::cout << std::endl;
-  std::cout << heap.toString(alg::TreeStyle::H_BOXED);
-  std::cout << std::endl;
+  std::array values = {1,22,16,4,46,29,11,37,7,2};
+  // alg::MedHeap<int, float> mheap(values.begin(), values.end());
+  // std::cout << std::endl;
+  // std::cout << "median: " << mheap.median();
+  // std::cout << std::endl;
+  // std::cout << mheap.toString();
+  // std::cout << std::endl;
+  alg::MedHeap<int, float> mheap;
+  for (auto const &v : values) {
+    mheap.insert(v);
+    std::cout << std::endl;
+    std::cout << "median: " << mheap.median();
+    std::cout << std::endl;
+    std::cout << mheap.toString();
+    std::cout << std::endl;
+  }
 
   return 0;
 }
