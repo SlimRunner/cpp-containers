ce85fec952f7ebedb8c03e41b82590c69f75b1ff
SlimRunner <dv.polygon@gmail.com>
2024-07-12 02:04:06 -0700

    Implement toString pre-formatter

--- include/HeapUtils.hpp
+++ include/HeapUtils.hpp
@@ -15,10 +15,77 @@
 #include <vector>
 
 namespace alg {
+
+enum class PrintOrientation {
+  HORIZONTAL, // levels are printed left-to-right or vice versa
+  VERTICAL,   // levels are printed top-to-bottom or viceversa
+};
+
+enum class PrintOrder {
+  TOP_DOWN,  // first print the root and go down the tree
+  BOTTOM_UP, // otherwise
+};
+
+enum class LevelOrder {
+  LEFT_TO_RIGHT, // tree is printed left-to-right
+  RIGHT_TO_LEFT, // otherwise
+};
+
+enum class LevelSpacing {
+  SPACED,              // levels are tabulated with spaces
+  BOX_DRAWING,         // levels are connected with box-drawing characters
+  BOX_DRAWING_ROUNDED, // same as box-drawing but with rounded corners
+  CUSTOM,              // levels are connected with a custom set of connectors
+};
+
+// box-drawing set is:
+// {─, │, ┌, ┐, └, ┘, ├, ┤, ┬, ┴, ┼, ╴, ╵, ╶, ╷}
+enum class DrawingSet {
+  horzPipe,
+  vertPipe,
+  seTurn,
+  swTurn,
+  neTurn,
+  nwTurn,
+  leftTee,
+  rightTee,
+  bottomTee,
+  topTee,
+  intersection,
+  horzPipeHalfLeft,
+  vertPipeHalfTop,
+  horzPipeHalfRight,
+  vertPipeHalfBottom,
+};
+
+enum class TreeDensity {
+  NORMAL, // connectors run in their own separate lines
+  COMPACT // each row/column contains a level
+};
+
+enum class DataPadding {
+  LEFT,
+  RIGHT,
+  INTERNAL, // used for padding negative numbers with 0s
+  CENTER,
+};
+
+struct TreePrintStyle {
+  PrintOrientation orientation;
+  PrintOrder order;
+  LevelOrder levelOrder;
+  LevelSpacing spacing;
+  TreeDensity density;
+  DataPadding padding;
+  std::array<char, 15> drawingSet;
+  char padChar = ' ';
+};
+
 enum class TreeStyle {
   V_SPACED,
   V_BOXED,
   H_TABBED,
   H_BOXED,
 };
+
 } // namespace alg
--- include/OrderedHeap.hpp
+++ include/OrderedHeap.hpp
@@ -18,8 +18,7 @@ public:
   std::vector<T>::iterator begin() { return mHeap.begin(); }
   std::vector<T>::iterator end() { return mHeap.end(); }
 
-  std::vector<std::string> toLines(TreeStyle style,
-                                    bool rounded = false) const;
+  std::vector<std::string> toLines(bool rounded = false) const;
 
 private:
   TreePrintStyle mStyle;
@@ -30,6 +29,12 @@ private:
   void balanceRoot();
   bool hasLeft(size_t index);
   bool hasRight(size_t index);
+
+  void getPrintDiagram(std::vector<size_t> &indices,
+                       std::vector<std::vector<bool>> &map) const;
+  std::vector<size_t> getLevelOrder() const;
+  std::vector<size_t> getPreOrder() const;
+  void preOrderHelper(size_t i, std::vector<size_t> &out) const;
 };
 
 template <class T, class Compare>
@@ -59,7 +64,7 @@ template <class T, class Compare> inline T OrderedHeap<T, Compare>::remove() {
 
 template <class T, class Compare>
 inline std::vector<std::string>
-OrderedHeap<T, Compare>::toLines(TreeStyle style, bool rounded) const {
+OrderedHeap<T, Compare>::toLines(bool rounded) const {
   const size_t HEIGHT =
       static_cast<size_t>(std::ceil(std::log2(mHeap.size() + 1)));
   const size_t HEAP_SIZE = mHeap.size();
@@ -102,9 +107,10 @@ OrderedHeap<T, Compare>::toLines(TreeStyle style, bool rounded) const {
         std::max<double>(spacing, offset + std::ceil(std::log10(num + 1))));
   }
 
-  switch (style) {
-  case TreeStyle::H_BOXED:
-  case TreeStyle::H_TABBED:
+  switch (mStyle.orientation) {
+  // case TreeStyle::H_BOXED:
+  // case TreeStyle::H_TABBED:
+  case PrintOrientation::HORIZONTAL:
     queue.push(0);
     enqeue(1);
 
@@ -120,8 +126,9 @@ OrderedHeap<T, Compare>::toLines(TreeStyle style, bool rounded) const {
     }
 
     break;
-  case TreeStyle::V_BOXED:
-  case TreeStyle::V_SPACED:
+  // case TreeStyle::V_BOXED:
+  // case TreeStyle::V_SPACED:
+  case PrintOrientation::VERTICAL:
     for (size_t lvl = 0, N0 = 0, N1 = 0; lvl < HEIGHT; ++lvl) {
       N1 = N0 + (1 << lvl);
       oss << std::setw(0);
@@ -200,6 +207,90 @@ inline bool OrderedHeap<T, Compare>::hasLeft(size_t index) {
 template <class T, class Compare>
 inline bool OrderedHeap<T, Compare>::hasRight(size_t index) {
   return (index + 1) * 2 + 1 >= mHeap.size();
+}
+
+template <class T, class Compare>
+inline void OrderedHeap<T, Compare>::getPrintDiagram(
+    std::vector<size_t> &indices, std::vector<std::vector<bool>> &map) const {
+  const size_t HEIGHT =
+      static_cast<size_t>(std::ceil(std::log2(mHeap.size() + 1)));
+  const size_t HEAP_SIZE = mHeap.size();
+  std::vector<std::vector<bool>> outDiag;
+  std::queue<size_t> queue;
+  std::function<void(size_t)> enqeue;
+
+  if (!HEAP_SIZE) {
+    return;
+  }
+
+  size_t levelRows, count;
+  size_t termOffset, delta; // terms of linear sequence
+
+  switch (mStyle.orientation) {
+  case PrintOrientation::HORIZONTAL:
+    indices = getPreOrder();
+    for (const auto &i : indices) {
+      auto lvl = static_cast<size_t>(std::ceil(std::log2(i + 2))) - 1;
+      map.push_back({});
+      for (size_t n = 0; n < HEIGHT; ++n) {
+        map.back().push_back(n == lvl);
+      }
+    }
+    break;
+  case PrintOrientation::VERTICAL:
+    delta = 1 << HEIGHT;
+    termOffset = (delta >> 1) - 1;
+    levelRows = delta - 1; // trim last gap
+    indices = getLevelOrder();
+
+    count = 0;
+    for (size_t lvl = 0; lvl < HEIGHT; ++lvl) {
+      map.push_back({});
+      for (size_t n = 0, ni; n < levelRows; ++n) {
+        ni = ((n - termOffset) / delta) * delta + termOffset;
+        map.back().push_back(n == ni && count < HEAP_SIZE);
+        count += map.back().back();
+      }
+      delta >>= 1;
+      termOffset = (delta >> 1) - 1;
+    }
+    break;
+
+  default:
+    break;
+  }
+}
+
+template <class T, class Compare>
+inline std::vector<size_t> OrderedHeap<T, Compare>::getLevelOrder() const {
+  std::vector<size_t> out;
+  for (size_t i = 0; i < mHeap.size(); ++i) {
+    out.push_back(i);
+  }
+  return out;
+}
+
+template <class T, class Compare>
+inline std::vector<size_t> OrderedHeap<T, Compare>::getPreOrder() const {
+  std::vector<size_t> out{0};
+  preOrderHelper(1, out);
+  return out;
+}
+
+template <class T, class Compare>
+inline void
+OrderedHeap<T, Compare>::preOrderHelper(size_t i,
+                                        std::vector<size_t> &out) const {
+  i <<= 1;
+  if (i <= mHeap.size()) {
+    out.push_back(i - 1);
+    preOrderHelper(i, out);
+  }
+  ++i;
+  if (i <= mHeap.size()) {
+    out.push_back(i - 1);
+    preOrderHelper(i, out);
+  }
 }
 
 } // namespace alg
